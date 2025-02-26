7b55349826d20579ac6681242de254859b161771
navnit.amcodr <navnit.amcodr@gmail.com>
2025-02-26 11:05:57 +0530

    job log query optimized

--- app/Http/Controllers/Admin/JobLogController.php
+++ app/Http/Controllers/Admin/JobLogController.php
@@ -24,13 +24,14 @@ class JobLogController extends Controller
     public function getData(Request $request)
     {
         if ($request->ajax()) {
-            $offers = JobLog::orderBy('created_at', 'desc')->get();
+            $offers = JobLog::query()
+            ->orderBy('created_at', 'desc');
 
             return DataTables::of($offers)
                 ->addIndexColumn()
                 ->addColumn('create_time', function ($row) {
                     return Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->format('d.m.Y H.i');
-                })                
+                })
                 ->make(true);
         }
     }
